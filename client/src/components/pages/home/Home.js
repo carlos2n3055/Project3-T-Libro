3bf686e23e3b52ef6de15c4ce33de0e2dc4811fe
Carlos Martin-Salas Larena <carlos_2n3055@hotmail.com>
2020-12-13 14:10:06 +0100

    Heyling merge Carlos

--- client/src/components/pages/home/Home.js
+++ client/src/components/pages/home/Home.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react'
 
 import { Col, Container, Row } from 'react-bootstrap'
-
 import './Home.css'
+
+import exchange from './exchange.jpeg'
+import sale from './sale.jpg'
+import comments from './comments.jpg'
+import register from './register.jpg'
 
 
 import BackgroundVideo from './../backgroundVideo/BackgroundVideo'
@@ -47,18 +51,20 @@ class Home extends Component {
 
                         <Col lg={6}>
 
-                            <div>
-                                <h4>Intercambia</h4>
-                                <p>Intercambia tus libros que ya te hayas leido y ocupan espacio, por aquellos que te llamen la atención </p>
-
-                                <img></img>
+                            <div className="align-items-center">
+                                <div>
+                                    <img src={exchange} />
+                                </div>
+                                <div>
+                                    <h4>Intercambia</h4>
+                                    <p>Intercambia tus libros que ya te hayas leido y ocupan espacio, por aquellos que te llamen la atención </p>
+                                </div>
                             </div>
 
                             <div>
+                                <img src={sale} />
                                 <h4>Vende</h4>
                                 <p>Vende al mejor precio los libros que ya no quieras tener en casa  </p>
-
-                                <img></img>
                             </div>
                         </Col>
                         
@@ -68,16 +74,15 @@ class Home extends Component {
 
                         <Col lg={6}>
                             <div>
+                                <img src={comments} />
                                 <h4>Comenta</h4>
                                 <p>Comparte con la comunidad tu opinion de los libros que ya has leido </p>
-                                <img></img>
                             </div>
 
                             <div>
+                                <img src={register} />
                                 <h4>Registrate</h4>
                                 <p>Para poder disfrutar de tu aplicacion tienes que registrarte y logearte </p>
-
-                                <img></img>
                             </div>
                         </Col>
 
